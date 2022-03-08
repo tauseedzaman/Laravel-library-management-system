3daabfb699294526b354139489cc1688c7d0e536
tauseedzaman <tauseedzamanofsorana@gmail.com>
2022-03-08 16:25:23 +0500

    login bug fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            AdminSeeder::class,
+            UserSeeder::class,
             AutherSeeder::class,
             BookIssueSeeder::class,
             CategorySeeder::class,
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class AdminSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
